ed51838dea44967116574ff7ec796b265610294a
Aaron Lun <alun@wehi.edu.au>
2017-03-30 10:43:57 +0100

    Updated UI for p-value app.

--- Day2/shiny/pvalue/reference.R
+++ Day2/shiny/pvalue/reference.R
@@ -1,6 +1,6 @@
 # Some reference values for use in both ui.R and server.R
 all.tests <- c("two-sided", "one-sided (upper)", "one-sided (lower)")
-all.hyp <- c("null", "alternative")
+all.hyp <- c("Null (everyone is healthy)", "Alternative (everyone is sick)")
 normal.temp <- 37
 normal.sd <- 0.5
 
